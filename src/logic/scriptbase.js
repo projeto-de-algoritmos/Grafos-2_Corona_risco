dbe305dbfd3fbc0fdef3dd4c75a89a62f66800fe
Pedro Lima <48936052+PedroLimass@users.noreply.github.com>
2021-03-07 14:22:22 -0300

    Mostrando pontos sem vinculo ainda

---
+++ src/logic/scriptbase.js
@@ -0,0 +1,20 @@
+var vertices = [];
+
+function setup() {
+  createCanvas(1400, 660);
+}
+
+function mousePressed() {
+  var v = createVector(mouseX, mouseY);
+  vertices.push(v);
+}
+
+function draw() {
+  background(158, 158, 158);
+
+  for (var i = 0; i < vertices.length; i++) {
+    fill(255);
+    stroke(255);
+    ellipse(vertices[i].x, vertices[i].y, 15, 15);
+  }
+}
